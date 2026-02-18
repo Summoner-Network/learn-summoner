ae30c009a636b062afefdc571be566361269df9d
Remy Tuyeras <rtuyeras@gmail.com>
2026-02-18 11:09:53 -0500

    add examples

---
+++ learn/example_2/agent.py
@@ -0,0 +1,50 @@
+import argparse, json
+from typing import Any
+
+from summoner.client import SummonerClient
+from summoner.protocol import Node, Move, Stay, Test, Event
+from summoner.visionary import ClientFlowVisualizer
+
+
+AGENT_ID = "VisionaryTestAgent"
+viz = ClientFlowVisualizer(title=f"{AGENT_ID} Graph", port=8710)
+
+client = SummonerClient(name=AGENT_ID)
+
+client_flow = client.flow().activate()
+client_flow.add_arrow_style(stem="-", brackets=("[", "]"), separator=",", tip=">")
+Trigger = client_flow.triggers()
+
+@client.receive(route="register")
+async def on_register(msg: Any) -> Event: 
+    client.logger.info(msg)
+    return Test(Trigger.ok)
+
+@client.receive(route="contact")
+async def on_register(msg: Any) -> Event: 
+    client.logger.info(msg)
+    return Test(Trigger.ok)
+
+@client.receive(route="friend")
+async def on_register(msg: Any) -> Event: 
+    client.logger.info(msg)
+    return Test(Trigger.ok)
+
+@client.receive(route="ban")
+async def on_register(msg: Any) -> Event: 
+    client.logger.info(msg)
+    return Test(Trigger.ok)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run a Summoner client with a specified config.")
+    parser.add_argument('--config', dest='config_path', required=False, help='The relative path to the config file (JSON) for the client (e.g., --config configs/client_config.json)')
+    args = parser.parse_args()
+
+    # Start visual window (browser) and build graph from dna
+    viz.attach_logger(client.logger)
+    viz.start(open_browser=True)
+    viz.set_graph_from_dna(json.loads(client.dna()), parse_route=client_flow.parse_route)
+    viz.push_states([Node("A")])
+
+    client.run(host = "127.0.0.1", port = 8888, config_path=args.config_path or "configs/client_config.json")
